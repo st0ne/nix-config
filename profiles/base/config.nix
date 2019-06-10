3caab82c662fbe8783eeadefa86dd3f58d3f2d71
Marcello Sylvester Bauer <sylvblck@sylv.io>
2019-06-10 23:10:08 +0200

    add gnupg path

--- profiles/base/config.nix
+++ profiles/base/config.nix
@@ -4,11 +4,17 @@
   system.activationScripts = {
   };
   system.userActivationScripts = {
+    gnupgSetup = {
+      text = '' mkdir -p /data/user/$USER/Auth/GnuPG/${config.general.name}
+                ln -sfn /data/user/$USER/Auth/GnuPG/${config.general.name} ~/.gnupg '';
+      deps = [];
+    };
     htopSetup = {
       text = '' mkdir -p /data/user/$USER/Config/htop/${config.general.name}
                 ln -sfn /data/user/$USER/Config/htop/${config.general.name} ~/.config/htop '';
       deps = [];
     };
+
     rangerSetup = {
       text = '' ln -sfn /data/user/$USER/Config/ranger ~/.config '';
       deps = [];
