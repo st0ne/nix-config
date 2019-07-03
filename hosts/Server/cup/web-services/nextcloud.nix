719dc18fae81754f398daa975f134094419fee7d
Marcello Sylvester Bauer <sylvblck@sylv.io>
2019-07-03 20:09:24 +0200

    add nginx and nextcloud

---
+++ hosts/Server/cup/web-services/nextcloud.nix
@@ -0,0 +1,48 @@
+{ config, pkgs, ... }:
+
+let
+  secret = import ../../../../secrets.nix {};
+  hostname = "${secret.nextcloud.hostname}.${secret.sylv.domain}";
+in {
+  services.nextcloud = {
+    enable = true;
+    hostName = hostname;
+    nginx.enable = true;
+    home = "/data/www/${hostname}/nextcloud";
+    autoUpdateApps.enable = true;
+    config = {
+      dbtype = "pgsql";
+      dbname = "nextcloud";
+      dbuser = "nextcloud";
+      dbpass = secret.nextcloud.dbpass;
+      adminuser = "admin";
+      adminpass = secret.nextcloud.adminpass;
+    };
+  };
+
+  services.nginx.virtualHosts."${hostname}" = {
+    # enable Let's Encrypt
+    enableACME = true;
+    forceSSL = true;
+    # security
+    # ref: https://docs.nextcloud.com/server/15/admin_manual/installation/harden_server.html
+    extraConfig = ''
+    add_header Strict-Transport-Security "max-age=31536000" always;
+    '';
+  };
+
+  services.postgresql = {
+    enable = true;
+    dataDir = "/data/www/${hostname}/postgres";
+    initialScript = pkgs.writeText "psql-init" ''
+      CREATE ROLE ${config.services.nextcloud.config.dbuser} WITH PASSWORD '${config.services.nextcloud.config.dbpass}' LOGIN;
+      CREATE DATABASE ${config.services.nextcloud.config.dbname} WITH OWNER ${config.services.nextcloud.config.dbuser};
+    '';
+  };
+
+  # ensure that postgres is running *before* running the setup
+  systemd.services."nextcloud-setup" = {
+    requires = ["postgresql.service"];
+    after = ["postgresql.service"];
+  };
+}
---
+++ hosts/Server/cup/web-services/nginx.nix
@@ -0,0 +1,42 @@
+{ config, lib, pkgs, ... }:
+
+let
+  secret = import ../../../../secrets.nix {};
+  ### redirect www subdomain
+  configRedirectWWW = ''
+  server {
+    server_name www.${secret.sylv.domain};
+	  # listen on ipv4
+    listen 443 ssl http2;
+	  # listen on ipv6
+	  listen [::]:443 ssl http2;
+	  return 301 https://${secret.sylv.domain};
+  }
+  '';
+in {
+  networking.firewall.allowedTCPPorts = [ 80 443 ];
+
+  services.nginx = {
+    enable = true;
+    # only recommendedProxySettings and recommendedGzipSettings are strictly required,
+    # but the rest make sense as well
+    recommendedTlsSettings = true;
+    recommendedOptimisation = true;
+    recommendedGzipSettings = true;
+    recommendedProxySettings = true;
+
+    appendHttpConfig = ''
+    ${configRedirectWWW}
+    '';
+
+    virtualHosts = {
+      "${secret.sylv.domain}" = {
+      enableACME = true;
+      forceSSL = true;
+        locations."/".extraConfig = ''
+          return 200;
+        '';
+      };
+    };
+  };
+}
