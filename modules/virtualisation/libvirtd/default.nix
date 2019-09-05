35d012fc8b7ad4dd1e6dde68aecc206fd6abf01f
Marcello Sylvester Bauer <sylvblck@sylv.io>
2019-09-05 14:44:04 -0700

    add libvirt data paths

--- modules/virtualisation/libvirtd/default.nix
+++ modules/virtualisation/libvirtd/default.nix
@@ -5,6 +5,23 @@
     enable = true;
   };
 
+  system.activationScripts = {
+    libvirtd = {
+      text = ''
+      mkdir -p /data/host/libvirtd/qemu
+      ln -sfn  /data/host/libvirtd/qemu /var/lib/libvirt/
+      mkdir -p /data/host/libvirtd/storage/autostart
+      ln -sfn  /data/host/libvirtd/storage /var/lib/libvirt/
+      mkdir -p /data/host/libvirtd/secrets
+      ln -sfn  /data/host/libvirtd/secrets /var/lib/libvirt/
+      mkdir -p /data/host/libvirtd/images
+      ln -sfn  /data/host/libvirtd/images /var/lib/libvirt/
+      mkdir -p /data/host/libvirtd/kernel
+      ln -sfn  /data/host/libvirtd/kernel /var/lib/libvirt/
+      '';
+      deps = [];
+    };
+  };
   environment = {
     systemPackages = with pkgs; [ virtmanager ];
     # Set the default connection string for `virsh` etc to be the system qemu instance.
