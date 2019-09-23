57e6c9bed56d11abd7b5e6a48705c6c28f2676ed
Marcello Sylvester Bauer <sylvblck@sylv.io>
2019-09-23 09:40:21 +0200

    sylv: add git config

--- users/sylv/full.nix
+++ users/sylv/full.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ config, ... }:
 
 {
   imports = [
@@ -7,6 +7,19 @@
   ];
 
   home-manager.users.sylv = {
+    programs = {
+      git = {
+        enable = true;
+        userName = config.users.users.sylv.description;
+        userEmail = "sylvblck@sylv.io";
+        aliases = {
+          # ref: https://stackoverflow.com/questions/1057564/pretty-git-branch-graphs
+          lg = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
+          adog = "log --all --decorate --oneline --graph";
+        };
+        ignores = [ "*~" "*.swp" ];
+      };
+    };
   };
 
 }
