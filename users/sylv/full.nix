5d916047ecc65b49070ceadeb8d8079ed826cec6
Marcello Sylvester Bauer <sylv@sylv.io>
2019-10-13 13:01:50 +0200

    change git email
    Signed-off-by: Marcello Sylvester Bauer <sylv@sylv.io>

--- users/sylv/full.nix
+++ users/sylv/full.nix
@@ -11,7 +11,7 @@
       git = {
         enable = true;
         userName = config.users.users.sylv.description;
-        userEmail = "sylvblck@sylv.io";
+        userEmail = "sylv@sylv.io";
         aliases = {
           # ref: https://stackoverflow.com/questions/1057564/pretty-git-branch-graphs
           lg = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
